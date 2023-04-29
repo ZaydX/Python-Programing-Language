5702d8628f29a9b2448268eb169d0351d2f30501
ZaydX <132158609+ZaydX@users.noreply.github.com>
2023-04-29 19:16:33 +0200

    Add files via upload

---
+++ bf.py
@@ -0,0 +1,22 @@
+def shootInFoot():
+    while True:
+        print("hahahahahah")
+        csihqa =nkg
+        print i
+        wga
+        shooyourself
+
+def supWrite(value):
+    print(value)
+
+def aStringQuestion(question, hereNull):
+    hereNull = input(str(question))
+
+def aStringQuestion(question, hereNull):
+    hereNull = input(str(question))
+
+def aIntegerQuestion(question, hereNull):
+    hereNull = input(int(question))
+
+def aBooleanQuestion(question, hereNull):
+    hereNull = input(bool(question))
